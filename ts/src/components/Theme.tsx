e9caa297af6ce23685dd35d26b9a6c5a879bc6f5
여다희 <yeodahui.dev@gmail.com>
2024-07-12 11:05:39 +0900

    feat: 다크모드 설정

--- ts/src/components/Theme.tsx
+++ ts/src/components/Theme.tsx
@@ -1,9 +1,16 @@
 function Theme() {
+  const toggleDarkMode = () => {
+    const $html = document.querySelector("html");
+
+    $html?.classList.toggle("dark");
+  };
+
   return (
     <button
       type="button"
       data-toggle-dark="dark"
       className="ml-4 flex items-center w-8 h-8 justify-center text-xs font-medium text-gray-700 bg-white border border-gray-200 rounded-lg toggle-dark-state-example hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-gray-300 dark:focus:ring-gray-500 dark:bg-gray-800 focus:outline-none dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+      onClick={toggleDarkMode}
     >
       <svg
         data-toggle-icon="moon"
